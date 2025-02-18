b5a32a2fa6f5d9da1d35fc0f2ae8ae353f470878
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-02-18 17:08:39 +0200

    Commit adding comments

--- lib/client.dart
+++ lib/client.dart
@@ -1,14 +1,17 @@
 import 'package:singleton_pattern/database_singleton.dart';
-
+// client class to check the equality of the data base created
 class Client {
 void checkDatabase(){
   DataBase d1 = DataBase.getInstance();
   DataBase d2 = DataBase.getInstance();
+  // Check if both instances are equal
+
   if (d1 == d2){
     print('Data base are equal');
   }
   else{
     print('Data base are not equal');
   }
+  d1.createDatabase();
 }
 }
--- lib/database_singleton.dart
+++ lib/database_singleton.dart
@@ -2,10 +2,12 @@ class DataBase
 {
   static  DataBase? _singleInstance;
 
+  // Private constructor to prevent instantiation.
   DataBase._internal() {
     print('Database single instance created');
   }
 
+  // Method to get the instance of the Database
   static DataBase getInstance()
   {
     _singleInstance ??= DataBase._internal();
